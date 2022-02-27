639ee4cb3e86c83a2ef323dafafd3f332793c3df
iyoshiha <yoshi3co@gmail.com>
2022-02-27 17:19:12 +0900

    multiple array

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,23 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String monthStr="t";
-		switch (monthStr) {
-			case "nice": monthStr = "January";
-					break;
-					
-			case "great":	monthStr = "February";
-					break;
-			case "sucks":	monthStr = "March";
-						
-					
-			case "brutal women":	monthStr = "April";
-			break;
-			
-			default: monthStr = "YOU WERE NOT BORN!!!";
+		String[] a  = {"abc", "xyz"};
+		String[][] ary = {{"abc", "klm"},null, {"def"}} ;
 				
-		}
-		System.out.println(monthStr);
+		
+	for (String[] b: ary)
+	{
+		System.out.println(b.length);
+	}
 	}
 
 }
